1456a163146873ee98189f5afd6c9d3369586b8f
Gregory Korsun <lvlup9119@gmail.com>
2025-02-27 21:08:46 +0200

    Create frontend folder(project) set up other things for corect work. Delete home folder(project)

--- Evently/Evently/urls.py
+++ Evently/Evently/urls.py
@@ -17,10 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from home.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("verification.urls")),
-	path('', home),
+    path("", include("frontend.urls"))
 ]
