eee08f88559d8a3ed354c40f8b8aee10ca978785
Hryhorii <102456972+hryhoriikorsun@users.noreply.github.com>
2025-03-04 20:04:35 +0200

    Feature/user page (#4)
    * Create home page.
    
    * Recode events.

--- Evently/frontend/views.py
+++ Evently/frontend/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render(request, "frontend/index.html")
+
+def events(request):
+  return render(request, "frontend/events.html")
+
+def home(request):
+  return render(request, "frontend/home.html")
